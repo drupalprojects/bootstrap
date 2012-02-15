0f6ae04a247437f24965814dd35344764c6116ce
andregriffin <andregriffin@gmail.com>
2012-02-15 16:56:49 -0500

    Change navbar to <header> element, since it includes site title, Only wrap Primary/Secondary Menu in <nav>

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,44 +1,47 @@
-<nav id="navbar" role="navigation" class="navbar navbar-fixed-top">
+<header id="navbar" role="navigation" class="navbar navbar-fixed-top">
   <div class="navbar-inner">
-	<div class="container">
-	  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-	  </a>
-	  
-	  <?php if ($logo): ?>
-		<a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-		  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-		</a>
-	  <?php endif; ?>
-	  <?php if ($site_name || $site_slogan): ?>
-		<hgroup id="site-name-slogan">
-		  <?php if ($site_name): ?>
-			<h1>
-			  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
-			</h1>
-		  <?php endif; ?>
-		</hgroup>
-	  <?php endif; ?>
-	  
-	  <div class="nav-collapse">
-		<?php if ($primary_nav): ?>
-		  <?php print $primary_nav; ?>
-		<?php endif; ?>
-	  
-		<?php if ($search): ?>
-		  <?php if ($search): print render($search); endif; ?>
-		<?php endif; ?>
-		
-		<?php if ($secondary_nav): ?>
-		  <?php print $secondary_nav; ?>
-		<?php endif; ?>
-	  </div>         
-	</div>
+  	<div class="container">
+  	  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+  	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+  		<span class="icon-bar"></span>
+  		<span class="icon-bar"></span>
+  		<span class="icon-bar"></span>
+  	  </a>
+  	  
+  	  <?php if ($logo): ?>
+    		<a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+    		  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+    		</a>
+  	  <?php endif; ?>
+
+  	  <?php if ($site_name || $site_slogan): ?>
+    		<hgroup id="site-name-slogan">
+    		  <?php if ($site_name): ?>
+    			<h1>
+    			  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
+    			</h1>
+    		  <?php endif; ?>
+    		</hgroup>
+  	  <?php endif; ?>
+  	  
+  	  <div class="nav-collapse">
+    	  <nav>
+      		<?php if ($primary_nav): ?>
+      		  <?php print $primary_nav; ?>
+      		<?php endif; ?>
+      	  
+      		<?php if ($search): ?>
+      		  <?php if ($search): print render($search); endif; ?>
+      		<?php endif; ?>
+      		
+      		<?php if ($secondary_nav): ?>
+      		  <?php print $secondary_nav; ?>
+      		<?php endif; ?>
+    		</nav>
+  	  </div>         
+  	</div>
   </div>
-</nav>
+</header>
 
 <div class="container">
 
