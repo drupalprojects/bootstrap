c8645f813d219cbf90bcd2dab9de833239902ea1
andregriffin <andregriffin@gmail.com>
2012-11-12 15:16:29 -0500

    Update support for logo to be placed within navbar

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -9,7 +9,7 @@
       </a>
       
       <?php if ($logo): ?>
-        <a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+        <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
