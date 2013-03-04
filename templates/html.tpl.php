7327ea5ffd585477db5fa11756343ef8eaf01786
Fabiano Sant'Ana <fabiano@chuva-inc.com>
2013-03-04 19:11:02 -0300

    Issue #1922580 by jesse.d: Remove protocol of html5shiv to allow both http and https

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -9,7 +9,7 @@
   <?php print $scripts; ?>
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
