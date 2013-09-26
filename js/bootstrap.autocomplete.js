527c3c7a5c2a1d9d94b4e8768bb240cd029e7599
el7cosmos <el7cosmos@251087.no-reply.drupal.org>
2013-09-25 22:53:03 -0500

    Issue #2097823 by el7cosmos: Style autocomplete drop downs

--- js/bootstrap.autocomplete.js
+++ js/bootstrap.autocomplete.js
@@ -25,6 +25,85 @@ Drupal.behaviors.autocomplete = {
   }
 };
 
+/**
+ * Highlights a suggestion.
+ */
+Drupal.jsAC.prototype.highlight = function (node) {
+  if (this.selected) {
+    $(this.selected).removeClass('active');
+  }
+  $(node).addClass('active');
+  this.selected = node;
+  $(this.ariaLive).html($(this.selected).html());
+};
+
+/**
+ * Unhighlights a suggestion.
+ */
+Drupal.jsAC.prototype.unhighlight = function (node) {
+  $(node).removeClass('active');
+  this.selected = false;
+  $(this.ariaLive).empty();
+};
+
+/**
+ * Positions the suggestions popup and starts a search.
+ */
+Drupal.jsAC.prototype.populatePopup = function () {
+  var $input = $(this.input);
+  // Show popup.
+  if (this.popup) {
+    $(this.popup).remove();
+  }
+  this.selected = false;
+  this.popup = $('<div class="dropdown"></div>')[0];
+  this.popup.owner = this;
+  $input.parent().after(this.popup);
+
+  // Do search.
+  this.db.owner = this;
+  this.db.search(this.input.value);
+};
+
+/**
+ * Fills the suggestion popup with any matches received.
+ */
+Drupal.jsAC.prototype.found = function (matches) {
+  // If no value in the textfield, do not show the popup.
+  if (!this.input.value.length) {
+    return false;
+  }
+
+  // Prepare matches.
+  var ul = $('<ul class="dropdown-menu"></ul>');
+  var ac = this;
+  ul.css({
+    display: 'block',
+    right: 0
+  });
+  for (key in matches) {
+    $('<li></li>')
+      .html($('<a href="#"></a>').html(matches[key]).click(function (e) { e.preventDefault(); }))
+      .mousedown(function () { ac.select(this); })
+      .mouseover(function () { ac.highlight(this); })
+      .mouseout(function () { ac.unhighlight(this); })
+      .data('autocompleteValue', key)
+      .appendTo(ul);
+  }
+
+  // Show popup with matches, if any.
+  if (this.popup) {
+    if (ul.children().length) {
+      $(this.popup).empty().append(ul).show();
+      $(this.ariaLive).html(Drupal.t('Autocomplete popup'));
+    }
+    else {
+      $(this.popup).css({ visibility: 'hidden' });
+      this.hidePopup();
+    }
+  }
+};
+
 Drupal.jsAC.prototype.setStatus = function (status) {
   var $throbber = $('.glyphicon-refresh', $('#' + this.input.id).parent());
 
