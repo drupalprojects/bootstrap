b5548a3d1f6b993866d79af95237f0b8950a80df
Mark Carver <mark.carver@me.com>
2013-09-28 18:16:59 -0500

    - Moved functions out of includes/menu.inc and removed it.

---
+++ theme/menu/menu-link.func.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @file
+ * menu-link.func.php
+ */
+
+/**
+ * Overrides theme_menu_link().
+ */
+function bootstrap_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    // Prevent dropdown functions from being added to management menu so it
+    // does not affect the navbar module.
+    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+      $sub_menu = drupal_render($element['#below']);
+    }
+    else {
+      // Add our own wrapper.
+      unset($element['#below']['#theme_wrappers']);
+      $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+      $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+      $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+
+      // Check if this element is nested within another.
+      if ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] > 1)) {
+        // Generate as dropdown submenu.
+        $element['#attributes']['class'][] = 'dropdown-submenu';
+      }
+      else {
+        // Generate as standard dropdown.
+        $element['#attributes']['class'][] = 'dropdown';
+        $element['#localized_options']['html'] = TRUE;
+        $element['#title'] .= ' <span class="caret"></span>';
+      }
+
+      // Set dropdown trigger element to # to prevent inadvertant page loading
+      // when a submenu link is clicked.
+      $element['#localized_options']['attributes']['data-target'] = '#';
+    }
+  }
+  // On primary navigation menu, class 'active' is not set on active menu item.
+  // @see https://drupal.org/node/1896674
+  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+    $element['#attributes']['class'][] = 'active';
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
--- theme/menu/menu-tree.func.php
+++ theme/menu/menu-tree.func.php
@@ -5,6 +5,13 @@
  */
 
 /**
+ * Overrides theme_menu_tree().
+ */
+function bootstrap_menu_tree(&$variables) {
+  return '<ul class="menu nav">' . $variables['tree'] . '</ul>';
+}
+
+/**
  * Bootstrap theme wrapper function for the primary menu links.
  */
 function bootstrap_menu_tree__primary(&$variables) {
