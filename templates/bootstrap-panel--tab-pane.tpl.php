76c297e7bbaddcb363e21a5177e0a569b75b0cdd
Mark Carver <mark.carver@me.com>
2013-09-27 21:49:30 -0500

    Wrapped tab-pane description in help-block.

--- templates/bootstrap-panel--tab-pane.tpl.php
+++ templates/bootstrap-panel--tab-pane.tpl.php
@@ -12,7 +12,9 @@
 <?php endif; ?>
 <div<?php print $attributes; ?>>
   <?php if ($description): ?>
+  <p class="help-block">
     <?php print $description; ?>
+  </p>
   <?php endif; ?>
   <?php print $content; ?>
 </div>
