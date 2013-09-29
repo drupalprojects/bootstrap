2879aa767d9d5109d57ced53d37c128062dd7623
Mark Carver <mark.carver@me.com>
2013-09-29 12:35:37 -0500

    Moved auto-rebuild of registry to includes/common.inc. Removed unnecessary properties in _bootstrap_theme().

--- template.php
+++ template.php
@@ -18,14 +18,6 @@
  * @see includes/registry.inc
  */
 
-// Auto-rebuild the theme registry during theme development.
-if (theme_get_setting('bootstrap_rebuild_registry') && !defined('MAINTENANCE_MODE')) {
-  // Rebuild .info data.
-  system_rebuild_theme_data();
-  // Rebuild theme registry.
-  drupal_theme_rebuild();
-}
-
 /**
  * Helper function for including theme files.
  *
