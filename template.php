47b943759041728cea76017e467ab8654129d7bf
Mark Carver <mark.carver@me.com>
2013-10-01 12:51:16 -0500

    Issue #2102287 by Mark Carver: Drush fatal errors when inside bootstrap directory

--- template.php
+++ template.php
@@ -6,16 +6,22 @@
  * This file should only contain light helper functions and stubs pointing to
  * other files containing more complex functions.
  *
- * The stubs should point to files within the `includes` folder named after
- * the function itself minus the theme prefix. If the stub contains a group
- * of functions, then please organize them so they are related in some way
- * and name the file appropriately to at least hint at what it contains.
+ * The stubs should point to files within the `theme` folder named after the
+ * function itself minus the theme prefix. If the stub contains a group of
+ * functions, then please organize them so they are related in some way and name
+ * the file appropriately to at least hint at what it contains.
  *
- * All [pre]process functions, theme function and template implementations
- * reside in the 'theme' folder. This is a highly automated and complex system
+ * All [pre]process functions, theme functions and template implementations also
+ * live in the 'theme' folder. This is a highly automated and complex system
  * designed to only load the necessary files when a given theme hook is invoked.
  * @see _bootstrap_theme()
- * @see includes/registry.inc
+ * @see theme/registry.inc
+ *
+ * Due to a bug in Drush, these includes must live inside the 'theme' folder
+ * instead of something like 'includes'. If a module or theme has an 'includes'
+ * folder, Drush will think it is trying to bootstrap core when it is invoked
+ * from inside the particular extension's directory.
+ * @see https://drupal.org/node/2102287
  */
 
 /**
@@ -50,14 +56,14 @@ function bootstrap_include($theme, $path) {
  * @see _bootstrap_theme()
  */
 function bootstrap_theme(&$existing, $type, $theme, $path) {
-  bootstrap_include($theme, 'includes/registry.inc');
+  bootstrap_include($theme, 'theme/registry.inc');
   return _bootstrap_theme($existing, $type, $theme, $path);
 }
 
 /**
  * Include common functions for theme.
  */
-bootstrap_include('bootstrap', 'includes/common.inc');
+bootstrap_include('bootstrap', 'theme/common.inc');
 
 /**
  * Declare various hook_*_alter() hooks.
@@ -65,4 +71,4 @@ bootstrap_include('bootstrap', 'includes/common.inc');
  * hook_*_alter() implementations must live (via include) inside this file so
  * they are properly detected when drupal_alter() is invoked.
  */
-bootstrap_include('bootstrap', 'includes/alter.inc');
+bootstrap_include('bootstrap', 'theme/alter.inc');
