419019bc2afe02afbeaa756669f3800cbe7bbbac
Mark Carver <mark.carver@me.com>
2013-10-04 16:14:44 -0500

    Minor coding standards cleanup.

--- theme/bootstrap/bootstrap-modal.tpl.php
+++ theme/bootstrap/bootstrap-modal.tpl.php
@@ -6,21 +6,20 @@
  * Markup for Bootstrap modals.
  *
  * Variables:
+ * - $attributes: Attributes for the outer modal div.
  * - $heading: Modal title.
  * - $body: The rendered body of the modal.
  * - $footer: The rendered footer of the modal.
- * - $attributes: Attributes for the outer modal div.
  */
-
 ?>
 <div<?php print $attributes; ?>>
-   <div class="modal-dialog">
-      <div class="modal-content">
-         <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h3><?php print $heading; ?></h3>
-         </div>
-         <div class="modal-body"><?php print $body; ?></div>
-         <div class="modal-footer"><?php print $footer; ?></div>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3><?php print $heading; ?></h3>
       </div>
-   </div>
+      <div class="modal-body"><?php print $body; ?></div>
+      <div class="modal-footer"><?php print $footer; ?></div>
+    </div>
+  </div>
 </div>
