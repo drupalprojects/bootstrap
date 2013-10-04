e1b68b4151b5def2c99a50b69d83970d62be2c71
valkum <valkum@457682.no-reply.drupal.org>
2013-10-04 16:59:46 -0500

    Issue #2102155 by valkum: Warning: array_search() expects parameter 2 to be array

--- theme/system/links.vars.php
+++ theme/system/links.vars.php
@@ -9,8 +9,17 @@
  */
 function bootstrap_preprocess_links(&$variables) {
   if (isset($variables['attributes']) && isset($variables['attributes']['class'])) {
+    $string = is_string($variables['attributes']['class']);
+    if($string) {
+      $variables['attributes']['class'] = explode(' ', $variables['attributes']['class']);
+    }
+
     if ($key = array_search('inline', $variables['attributes']['class'])) {
       $variables['attributes']['class'][$key] = 'list-inline';
+    }
+
+    if($string) {
+      $variables['attributes']['class'] = join(' ', $variables['attributes']['class']);
     }
   }
 }
