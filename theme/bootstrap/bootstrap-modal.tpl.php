1c5a61d87160af1f724f8ac9de2d5894bf333513
el7cosmos <el7cosmos@251087.no-reply.drupal.org>
2013-10-15 00:51:43 -0600

    Issue #2104185 by el7cosmos, valkum, bgilhome: Update bootstrap_modal theme hook and convert to template file

--- theme/bootstrap/bootstrap-modal.tpl.php
+++ theme/bootstrap/bootstrap-modal.tpl.php
@@ -15,8 +15,9 @@
 <div<?php print $attributes; ?>>
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h3><?php print $heading; ?></h3>
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title"><?php print $heading; ?></h4>
       </div>
       <div class="modal-body"><?php print $body; ?></div>
       <div class="modal-footer"><?php print $footer; ?></div>
--- theme/bootstrap/bootstrap-modal.vars.php
+++ theme/bootstrap/bootstrap-modal.vars.php
@@ -7,15 +7,26 @@
 /**
  * Implements theme_preprocess_bootstrap_modal().
  *
- * @todo: Implement the fade effect.
+ * @todo: Replace with "bootstrap_effect_fade" theme setting.
  */
 function bootstrap_preprocess_bootstrap_modal(&$variables) {
-  $element = &$variables['element'];
-  $attributes = !empty($element['#attributes']) ? $element['#attributes'] : array();
+  if (empty($variables['attributes']['id'])) {
+    $variables['attributes']['id'] = drupal_html_id(strip_tags($variables['heading']));
+  }
+  $variables['attributes']['class'][] = 'modal';
+  $variables['attributes']['class'][] = 'fade';
+  $variables['attributes']['tabindex'] = -1;
+  $variables['attributes']['role'] = 'dialog';
+  $variables['attributes']['aria-hidden'] = 'true';
 
-  $attributes['class'][] = 'modal';
-  $attributes['id'] = $variables['id'];
-  $variables['attributes'] = drupal_attributes($attributes);
+  $variables['heading'] = $variables['html_heading'] ? $variables['heading'] : check_plain($variables['heading']);
+}
 
-  $variables['heading'] = (isset($variables['html_heading']) && $variables['html_heading']) ? $variables['heading'] : check_plain($variables['heading']);
+/**
+ * Implements theme_process_bootstrap_modal().
+ */
+function bootstrap_process_bootstrap_modal(&$variables) {
+  $variables['attributes'] = drupal_attributes($variables['attributes']);
+  $variables['body'] = render($variables['body']);
+  $variables['footer'] = render($variables['footer']);
 }
