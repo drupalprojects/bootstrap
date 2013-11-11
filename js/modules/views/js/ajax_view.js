0f67e3baca27e1d2615bef2ca90d51e2bd34a9c1
valkum <valkum@457682.no-reply.drupal.org>
2013-11-11 17:24:41 -0600

    Issue #1907472 by valkum: Pager in views with ajax.

--- js/modules/views/js/ajax_view.js
+++ js/modules/views/js/ajax_view.js
@@ -5,7 +5,7 @@
  * Attach the ajax behavior to each link.
  */
 Drupal.views.ajaxView.prototype.attachPagerAjax = function() {
-  this.$view.find('ul.pager > li > a, th.views-field a, .attachment .views-summary a, .pagination ul li a')
+  this.$view.find('ul.pager > li > a, th.views-field a, .attachment .views-summary a, ul.pagination li a')
   .each(jQuery.proxy(this.attachPagerLinkAjax, this));
 };
 
