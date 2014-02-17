51ab0d221363fd3148cec50931074a41e1cbf865
Mark Carver <mark.carver@me.com>
2014-02-17 01:30:36 -0600

    Added theming elements for tableDrag

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -163,4 +163,15 @@ var Drupal = Drupal || {};
     }
   };
 
+  /**
+   * Tabledrag theming elements.
+   */
+  Drupal.theme.tableDragChangedMarker = function () {
+    return '<span class="tabledrag-changed glyphicon glyphicon-asterisk text-warning"></span>';
+  };
+
+  Drupal.theme.tableDragChangedWarning = function () {
+    return '<div class="tabledrag-changed-warning alert alert-warning messages warning">' + Drupal.theme('tableDragChangedMarker') + ' ' + Drupal.t('Changes made in this table will not be saved until the form is submitted.') + '</div>';
+  };
+
 })(jQuery, Drupal);
