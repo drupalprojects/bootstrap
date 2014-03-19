2749994ce02801c91dc3dd93d437a1374655ce47
Mark Carver <mark.carver@me.com>
2014-03-19 09:48:57 -0500

    Issue #2221327 by Mark Carver: Add "data-target" and "autoclose" options for popovers.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -57,11 +57,36 @@ var Drupal = Drupal || {};
   Drupal.behaviors.bootstrapPopovers = {
     attach: function (context, settings) {
       if (settings.bootstrap && settings.bootstrap.popoverEnabled) {
-        var elements = $(context).find('[data-toggle="popover"]').toArray();
+        var $currentPopover = $();
+        if (settings.bootstrap.popoverOptions.triggerAutoclose) {
+          $(document).on('click', function (e) {
+            if ($currentPopover.length && !$(e.target).is('[data-toggle=popover]') && $(e.target).parents('.popover.in').length === 0) {
+              $currentPopover.popover('hide');
+              $currentPopover = $();
+            }
+          });
+        }
+        var elements = $(context).find('[data-toggle=popover]').toArray();
         for (var i = 0; i < elements.length; i++) {
           var $element = $(elements[i]);
-          var options = $.extend(true, {}, settings.bootstrap.popoverOptions, $element.data());
-          $element.popover(options);
+          var options = $.extend({}, settings.bootstrap.popoverOptions, $element.data());
+          if (!options.content) {
+            options.content = function () {
+              var target = $(this).data('target');
+              return target && $(target) && $(target).length && $(target).clone().removeClass('element-invisible').wrap('<div/>').parent()[$(this).data('bs.popover').options.html ? 'html' : 'text']() || '';
+            }
+          }
+          $element.popover(options).on('click', function (e) {
+            e.preventDefault();
+          });
+          if (settings.bootstrap.popoverOptions.triggerAutoclose) {
+            $element.on('show.bs.popover', function () {
+              if ($currentPopover.length) {
+                $currentPopover.popover('hide');
+              }
+              $currentPopover = $(this);
+            });
+          }
         }
       }
     }
@@ -76,7 +101,7 @@ var Drupal = Drupal || {};
         var elements = $(context).find('[data-toggle="tooltip"]').toArray();
         for (var i = 0; i < elements.length; i++) {
           var $element = $(elements[i]);
-          var options = $.extend(true, {}, settings.bootstrap.tooltipOptions, $element.data());
+          var options = $.extend({}, settings.bootstrap.tooltipOptions, $element.data());
           $element.tooltip(options);
         }
       }
