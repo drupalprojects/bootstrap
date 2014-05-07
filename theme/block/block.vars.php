2219db2581033f46f46b9ff84d36d65856019428
m1r1k <m1r1k@1835386.no-reply.drupal.org>
2014-05-07 10:51:29 -0500

    Issue #2260775 by m1r1k: Update block processing.

--- theme/block/block.vars.php
+++ theme/block/block.vars.php
@@ -9,16 +9,10 @@
  */
 function bootstrap_preprocess_block(&$variables) {
   // Use a bare template for the page's main content.
-  if ($variables['block_html_id'] == 'block-system-main') {
+  if ($variables['plugin_id'] == 'system_main') {
     $variables['theme_hook_suggestions'][] = 'block__no_wrapper';
   }
-  $variables['title_attributes_array']['class'][] = 'block-title';
+  $variables['title_attributes']['class'][] = 'block-title';
+  $variables['attributes']['class'][] = 'clearfix';
 }
 
-/**
- * Implements hook_process_block().
- */
-function bootstrap_process_block(&$variables) {
-  // Drupal 7 should use a $title variable instead of $block->subject.
-  $variables['title'] = $variables['block']->subject;
-}
