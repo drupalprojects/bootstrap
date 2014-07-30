52d4e8755ef24afd52db6f1c4a529f84eaa13730
Mark Carver <mark.carver@me.com>
2014-07-30 03:14:02 -0500

    Fatal error: [] operator not supported for strings in ./templates/system/image.vars.php

--- templates/system/image.vars.php
+++ templates/system/image.vars.php
@@ -8,10 +8,18 @@
  * Implements hook_preprocess_image().
  */
 function bootstrap_preprocess_image(&$variables) {
+  // Contrib modules have a very bad habit of frequently adding classes as
+  // strings, convert them to a proper array.
+  // @see https://www.drupal.org/node/2269653
+  if (isset($variables['attributes']['class']) && !is_array($variables['attributes']['class'])) {
+    $variables['attributes']['class'] = explode(' ', $variables['attributes']['class']);
+  }
+
   // Add image shape, if necessary.
   if ($shape = theme_get_setting('bootstrap_image_shape')) {
     $variables['attributes']['class'][] = $shape;
   }
+
   // Add responsiveness, if necessary.
   if ($shape = theme_get_setting('bootstrap_image_responsive')) {
     $variables['attributes']['class'][] = 'img-responsive';
