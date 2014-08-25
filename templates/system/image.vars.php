df035926a88b71c85b9fccc04e6ae4501ddf52d0
Mark Carver <mark.carver@me.com>
2014-08-25 17:51:16 -0500

    Issue #2327547 by Mark Carver: Fixed img-responsive class is added twice.

--- templates/system/image.vars.php
+++ templates/system/image.vars.php
@@ -8,20 +8,13 @@
  * Implements hook_preprocess_image().
  */
 function bootstrap_preprocess_image(&$variables) {
-  // Contrib modules have a very bad habit of frequently adding classes as
-  // strings, convert them to a proper array.
-  // @see https://www.drupal.org/node/2269653
-  if (isset($variables['attributes']['class']) && !is_array($variables['attributes']['class'])) {
-    $variables['attributes']['class'] = explode(' ', $variables['attributes']['class']);
-  }
-
   // Add image shape, if necessary.
   if ($shape = theme_get_setting('bootstrap_image_shape')) {
-    $variables['attributes']['class'][] = $shape;
+    _bootstrap_add_class($shape, $variables);
   }
 
   // Add responsiveness, if necessary.
-  if ($shape = theme_get_setting('bootstrap_image_responsive')) {
-    $variables['attributes']['class'][] = 'img-responsive';
+  if (theme_get_setting('bootstrap_image_responsive')) {
+    _bootstrap_add_class('img-responsive', $variables);
   }
 }
