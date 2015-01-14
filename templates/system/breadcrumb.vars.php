3b2c2d6fe32c25801f56d58009a0ad9d07782f73
mkhamash <mkhamash@719596.no-reply.drupal.org>
2015-01-14 15:42:07 -0600

    Issue #2407245 by mkhamash: bootstrap breadcrumb add trailing slash if title is empty

--- templates/system/breadcrumb.vars.php
+++ templates/system/breadcrumb.vars.php
@@ -18,10 +18,14 @@ function bootstrap_preprocess_breadcrumb(&$variables) {
 
   if (theme_get_setting('bootstrap_breadcrumb_title') && !empty($breadcrumb)) {
     $item = menu_get_item();
-    $breadcrumb[] = array(
-      // If we are on a non-default tab, use the tab's title.
-      'data' => !empty($item['tab_parent']) ? check_plain($item['title']) : drupal_get_title(),
-      'class' => array('active'),
-    );
+
+    $page_title = !empty($item['tab_parent']) ? check_plain($item['title']) : drupal_get_title();
+    if (!empty($page_title)) {
+      $breadcrumb[] = array(
+        // If we are on a non-default tab, use the tab's title.
+        'data' => $page_title,
+        'class' => array('active'),
+      );
+    }
   }
 }
