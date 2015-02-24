e6b4273137f9d12afc405978590c9da10fcfd4f7
neardark <neardark@661076.no-reply.drupal.org>
2015-02-24 16:22:37 -0700

    Issue #2435079 by neardark: Update form-element-label preprocess

--- templates/system/form-element-label.vars.php
+++ templates/system/form-element-label.vars.php
@@ -7,39 +7,13 @@
 use Drupal\Component\Utility\Xss;
 
 /**
- * Overrides theme_form_element_label().
+ * Overrides preprocess_form_element_label().
  */
 function bootstrap_preprocess_form_element_label(&$variables) {
-  $element = $variables['element'];
-  // If title and required marker are both empty, output no label.
-  $variables['title'] = !empty($element['#title']) ? Xss::filterAdmin($element['#title']) : '';
-  $variables['attributes'] = array();
-
-  // Pass elements title_display to template.
-  $variables['title_display'] = $element['#title_display'];
-
-  // A #for property of a dedicated #type 'label' element as precedence.
-  if (!empty($element['#for'])) {
-    $variables['attributes']['for'] = $element['#for'];
-    // A custom #id allows the referenced form input element to refer back to
-    // the label element; e.g., in the 'aria-labelledby' attribute.
-    if (!empty($element['#id'])) {
-      $variables['attributes']['id'] = $element['#id'];
-    }
-  }
-  // Otherwise, point to the #id of the form input element.
-  elseif (!empty($element['#id'])) {
-    $variables['attributes']['for'] = $element['#id'];
-  }
-
-  // Pass elements required to template.
-  $variables['required'] = !empty($element['#required']) ? $element['#required'] : NULL;
-
   // Add generic Bootstrap identifier class.
   $variables['attributes']['class'][] = 'control-label';
 
   // Add description.
-  // @todo: We don't want to always do this.
   if (!empty($element['#description'])) {
     $variables['description'] = $element['#description'];
   }
