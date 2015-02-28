ed3c8a3d564667e8c4ce94929d14351b257d7d7c
neardark <neardark@661076.no-reply.drupal.org>
2015-02-28 11:37:36 -0700

    Issue #2443423 by neardark: Update ajax_view.js for Bootstrap

--- js/core/modules/views/js/ajax_view.js
+++ js/core/modules/views/js/ajax_view.js
@@ -1,12 +1,14 @@
 (function ($) {
 
-/**
- * Override Views prototype function so it can recognize Bootstrap AJAX pagers.
- * Attach the ajax behavior to each link.
- */
-Drupal.views.ajaxView.prototype.attachPagerAjax = function() {
-  this.$view.find('ul.pager > li > a, th.views-field a, .attachment .views-summary a, ul.pagination li a')
-  .each(jQuery.proxy(this.attachPagerLinkAjax, this));
-};
+  /**
+   * Override Views prototype function so it can recognize Bootstrap AJAX pagers.
+   * Attach the ajax behavior to each link.
+   */
+  if (typeof Drupal.views !== 'undefined') {
+    Drupal.views.ajaxView.prototype.attachPagerAjax = function() {
+      this.$view.find('ul.pager__items > li > a, th.views-field a, .attachment .views-summary a, ul.pagination li a')
+        .each(jQuery.proxy(this.attachPagerLinkAjax, this));
+    };
+  }
 
 })(jQuery);
