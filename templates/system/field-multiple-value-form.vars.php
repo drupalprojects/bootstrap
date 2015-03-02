20b756ea73e98def12a7bf4f74081c54879b009f
neardark <neardark@661076.no-reply.drupal.org>
2015-03-02 10:40:15 -0700

    Issue #2444487 by neardark: Theme multiple value form fields

---
+++ templates/system/field-multiple-value-form.vars.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @file
+ * form-multiple-value-form.vars.php
+ */
+
+use Drupal\Core\Template\Attribute;
+
+/**
+ * Implements hook_preprocess().
+ *
+ * @see template_preprocess_field_multiple_value_form().
+ */
+function bootstrap_preprocess_field_multiple_value_form(&$variables) {
+  $element = $variables['element'];
+
+  // Wrap header columns in label element for Bootstrap.
+  if ($variables['multiple']) {
+    $header_attributes = new Attribute(array('class' => array('label')));
+    $header = array(
+      array(
+        'data' => array(
+          '#prefix' => '<label' . $header_attributes . '>',
+          'title' => array(
+            '#markup' => t($element['#title']),
+          ),
+          '#suffix' => '</label>',
+        ),
+        'colspan' => 2,
+        'class' => array('field-label'),
+      ),
+      t('Order', array(), array('context' => 'Sort order')),
+    );
+
+    $variables['table']['#header'] = $header;
+  }
+}
