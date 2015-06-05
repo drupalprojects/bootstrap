ecef20ee30ce8e1338d1a950460d5950a701ab66
Mark Carver <mark.carver@me.com>
2015-06-05 16:30:20 -0500

    Issue #2501275: Convert deprecated attributes in html.tpl.php

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -10,6 +10,16 @@
  *   $language->language contains its textual representation.
  *   $language->dir contains the language direction. It will either be 'ltr' or
  *   'rtl'.
+ * - $html_attributes:  String of attributes for the html element. It can be
+ *   manipulated through the variable $html_attributes_array from preprocess
+ *   functions.
+ * - $html_attributes_array: An array of attribute values for the HTML element.
+ *   It is flattened into a string within the variable $html_attributes.
+ * - $body_attributes:  String of attributes for the BODY element. It can be
+ *   manipulated through the variable $body_attributes_array from preprocess
+ *   functions.
+ * - $body_attributes_array: An array of attribute values for the BODY element.
+ *   It is flattened into a string within the variable $body_attributes.
  * - $rdf_namespaces: All the RDF namespace prefixes used in the HTML document.
  * - $grddl_profile: A GRDDL profile allowing agents to extract the RDF data.
  * - $head_title: A modified version of the page title, for use in the TITLE
@@ -44,8 +54,9 @@
  * @ingroup themeable
  */
 ?><!DOCTYPE html>
-<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
-<head profile="<?php print $grddl_profile; ?>">
+<html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
+<head>
+  <link rel="profile" href="<?php print $grddl_profile; ?>" />
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
@@ -57,7 +68,7 @@
   <![endif]-->
   <?php print $scripts; ?>
 </head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<body<?php print $body_attributes; ?>>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
