cadc2c2d8b97c2e78b8256f56dee5a29e13cc6b9
rang501 <rang501@235669.no-reply.drupal.org>
2015-11-09 06:34:30 -0600

    Issue #2317763 by rang501, rroblik: Add modal dialog class size variable

--- templates/bootstrap/bootstrap-modal.tpl.php
+++ templates/bootstrap/bootstrap-modal.tpl.php
@@ -7,15 +7,19 @@
  *
  * Variables:
  * - $attributes: Attributes for the outer modal div.
+ * - $dialog_attributes: Attributes for the inner modal div.
  * - $heading: Modal title.
  * - $body: The rendered body of the modal.
  * - $footer: The rendered footer of the modal.
+ * - $size: The size of the modal. Can be empty, "sm" or "lg". This is
+ *   automatically added in the $dialog_attributes. See
+ *   bootstrap_preprocess_bootstrap_modal().
  *
  * @ingroup templates
  */
 ?>
 <div<?php print $attributes; ?>>
-  <div class="modal-dialog">
+  <div<?php print $dialog_attributes; ?>>
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
