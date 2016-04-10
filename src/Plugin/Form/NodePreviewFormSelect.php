c4e032af753c65d44afe40250cd8b000baf23f79
Mark Carver <mark.carver@me.com>
2016-04-10 16:00:21 -0500

    Issue #2703207 by vibrasphere: Preview mode deletes all content

--- src/Plugin/Form/NodePreviewFormSelect.php
+++ src/Plugin/Form/NodePreviewFormSelect.php
@@ -26,13 +26,23 @@ class NodePreviewFormSelect extends FormBase {
     $e->addClass(['form-inline', 'bg-info', 'text-center', 'clearfix']);
 
     // Backlink.
-    if ($options = $e->backlink->getProperty('options')) {
-      $e->backlink->addClass(isset($options['attributes']['class']) ? $options['attributes']['class'] : []);
-      $e->backlink->unsetProperty('options');
-    }
+    $options = $e->backlink->getProperty('options', []);
+
+    $e->backlink->addClass(isset($options['attributes']['class']) ? $options['attributes']['class'] : []);
     $e->backlink->addClass(['btn', 'btn-info', 'pull-left']);
     $e->backlink->setButtonSize();
     $e->backlink->setIcon(Bootstrap::glyphicon('chevron-left'));
+
+    // Ensure the UUID is set.
+    if ($uuid = $e->uuid->getProperty('value')) {
+      $options['query'] = ['uuid' => $uuid];
+    }
+
+    // Override the options attributes.
+    $options['attributes'] = $e->backlink->getAttributes()->getArrayCopy();
+
+    $e->backlink->setProperty('options', $options);
+
 
     // View mode.
     $e->view_mode->addClass('pull-right', $e::WRAPPER);
