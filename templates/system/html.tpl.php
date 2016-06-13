337f373d84d400219834f526b5ad87ab5bcb50ab
Mark Carver <mark.carver@me.com>
2016-06-13 07:37:01 -0500

    Issue #2724195 by psebborn: Bootstrap relies on Googlecode for HTML5Shiv, which no longer exists

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -64,7 +64,7 @@
   <?php print $styles; ?>
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
-    <script src="//cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
   <![endif]-->
   <?php print $scripts; ?>
 </head>
