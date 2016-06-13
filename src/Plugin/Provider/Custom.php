263f6057437e144365755fbcb80b453ad02765df
joegraduate <joegraduate@789384.no-reply.drupal.org>
2016-06-13 07:20:02 -0500

    Issue #2723829 by joegraduate: Custom CDN provider plugin does not load assets

--- src/Plugin/Provider/Custom.php
+++ src/Plugin/Provider/Custom.php
@@ -24,6 +24,13 @@ class Custom extends ProviderBase {
    */
   public function getAssets($types = NULL) {
     $this->assets = [];
+
+    // If no type is set, return all CSS and JS.
+    if (!isset($types)) {
+      $types = ['css', 'js'];
+    }
+    $types = is_array($types) ? $types : [$types];
+
     foreach ($types as $type) {
       if ($setting = $this->theme->getSetting('cdn_custom_' . $type)) {
         $this->assets[$type][] = $setting;
