7aac82ad3014511cd2a2b51eada933c153621821
bmoresafety20 <bmoresafety20@1175666.no-reply.drupal.org>
2016-10-13 12:49:19 -0500

    Issue #2810529 by kerasai: CTools Modal Loading Class Not Applied

--- js/modules/ctools/js/modal.js
+++ js/modules/ctools/js/modal.js
@@ -100,7 +100,7 @@
 
     $('.modal-title', Drupal.CTools.Modal.modal).html(Drupal.CTools.Modal.currentSettings.loadingText);
     Drupal.CTools.Modal.modalContent(Drupal.CTools.Modal.modal, settings.modalOptions, settings.animation, settings.animationSpeed);
-    $('#modalContent .modal-body').html(Drupal.theme(settings.throbberTheme));
+    $('#modalContent .modal-body').html(Drupal.theme(settings.throbberTheme)).addClass('ctools-modal-loading');
   };
 
   /**
