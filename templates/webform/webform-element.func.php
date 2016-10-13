8b6ff050cae10441b7ffefa69e78ac6f476819a1
seworthi <seworthi@325316.no-reply.drupal.org>
2016-10-13 12:53:47 -0500

    Issue #2803835 by seworthi, mjkovacevich: Undefined index in bootstrap_webform_element() when viewing a webform submission

--- templates/webform/webform-element.func.php
+++ templates/webform/webform-element.func.php
@@ -14,7 +14,7 @@ function bootstrap_webform_element(&$variables) {
   $element = &$variables['element'];
 
   // Inline title.
-  if ($element['#title_display'] === 'inline') {
+  if (isset($element['#title_display']) && $element['#title_display'] === 'inline') {
     $element['#title_display'] = 'before';
     $element['#wrapper_attributes']['class'][] = 'form-inline';
   }
