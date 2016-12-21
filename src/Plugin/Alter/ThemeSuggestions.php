716005a265718e434ac79d4506f470a6a1736e6b
Mark Carver <mark.carver@me.com>
2016-12-20 21:57:22 -0600

    Issue #2838071 by markcarver, drupalstrap: Allow ThemeSuggestion to be more dynamic

--- src/Plugin/Alter/ThemeSuggestions.php
+++ src/Plugin/Alter/ThemeSuggestions.php
@@ -7,6 +7,7 @@
 namespace Drupal\bootstrap\Plugin\Alter;
 
 use Drupal\bootstrap\Annotation\BootstrapAlter;
+use Drupal\bootstrap\Bootstrap;
 use Drupal\bootstrap\Plugin\PluginBase;
 use Drupal\bootstrap\Utility\Unicode;
 use Drupal\bootstrap\Utility\Variables;
@@ -22,53 +23,139 @@ use Drupal\Core\Entity\EntityInterface;
 class ThemeSuggestions extends PluginBase implements AlterInterface {
 
   /**
+   * @var array
+   */
+  protected $bootstrapPanelTypes = ['details', 'fieldset'];
+
+  /**
+   * An element object provided in the variables array, may not be set.
+   *
+   * @var \Drupal\bootstrap\Utility\Element|false
+   */
+  protected $element;
+
+  /**
+   * The theme hook invoked.
+   *
+   * @var string
+   */
+  protected $hook;
+
+  /**
+   * The theme hook suggestions, exploded by the "__" delimiter.
+   *
+   * @var array
+   */
+  protected $hookSuggestions;
+
+  /**
+   * The types of elements to ignore for the "input__form_control" suggestion.
+   *
+   * @var array
+   */
+  protected $ignoreFormControlTypes = ['checkbox', 'hidden', 'radio'];
+
+  /**
+   * The original "hook" value passed via hook_theme_suggestions_alter().
+   *
+   * @var string
+   */
+  protected $originalHook;
+
+  /**
+   * The array of suggestions to return.
+   *
+   * @var array
+   */
+  protected $suggestions;
+
+  /**
+   * The variables array object passed via hook_theme_suggestions_alter().
+   *
+   * @var \Drupal\bootstrap\Utility\Variables
+   */
+  protected $variables;
+
+  /**
    * {@inheritdoc}
    */
-  public function alter(&$suggestions, &$context1 = NULL, &$hook = NULL) {
-    $variables = Variables::create($context1);
+  public function alter(&$suggestions, &$variables = [], &$hook = NULL) {
+    // This is intentionally backwards. The "original" theme hook is actually
+    // the hook being invoked. The provided $hook (to the alter) is the watered
+    // down version of said original hook.
+    $this->hook = !empty($variables['theme_hook_original']) ? $variables['theme_hook_original'] : $hook;
+    $this->hookSuggestions = explode('__', $this->hook);
+    $this->originalHook = $hook;
+    $this->suggestions = $suggestions;
+    $this->variables = Variables::create($variables);
+    $this->element = $this->variables->element;
 
-    switch ($hook) {
-      case 'links':
-        if (Unicode::strpos($variables['theme_hook_original'], 'links__dropbutton') !== FALSE) {
-          // Handle dropbutton "subtypes".
-          // @see \Drupal\bootstrap\Plugin\Prerender\Dropbutton::preRenderElement()
-          if ($suggestion = Unicode::substr($variables['theme_hook_original'], 17)) {
-            $suggestions[] = 'bootstrap_dropdown' . $suggestion;
-          }
-          $suggestions[] = 'bootstrap_dropdown';
-        }
-        break;
+    // Processes the necessary theme hook suggestions.
+    $this->processSuggestions();
 
-      case 'fieldset':
-      case 'details':
-        if ($variables->element && $variables->element->getProperty('bootstrap_panel', TRUE)) {
-          $suggestions[] = 'bootstrap_panel';
-        }
-        break;
+    // Ensure the list of suggestions is unique.
+    $suggestions = array_unique($this->suggestions);
+  }
 
-      case 'input':
-        if ($variables->element && $variables->element->isButton()) {
-          if ($variables->element->getProperty('dropbutton')) {
-            $suggestions[] = 'input__button__dropdown';
-          }
-          else {
-            $suggestions[] = $variables->element->getProperty('split') ? 'input__button__split' : 'input__button';
-          }
-        }
-        elseif ($variables->element && !$variables->element->isType(['checkbox', 'hidden', 'radio'])) {
-          $suggestions[] = 'input__form_control';
-        }
-        break;
+  /***************************************************************************
+   * Dynamic alter methods.
+   ***************************************************************************/
 
-      // Add the "user" entity theme hook suggestions.
-      // @see https://www.drupal.org/node/2828634
-      // @see https://www.drupal.org/node/2808481
-      // @todo Remove/refactor once core issue is resolved.
-      case 'user':
-        $this->addEntitySuggestions($suggestions, $variables, 'user');
-        break;
+  /**
+   * Dynamic alter method for "input".
+   */
+  protected function alterInput() {
+    if ($this->element && $this->element->isButton()) {
+      $hook = 'input__button';
+      if ($this->element->getProperty('dropbutton')) {
+        $hook .= '__dropdown';
+      }
+      elseif ($this->element->getProperty('split')) {
+        $hook .= '__split';
+      }
+      $this->addSuggestion($hook);
     }
+    elseif ($this->element && !$this->element->isType($this->ignoreFormControlTypes)) {
+      $this->addSuggestion('input__form_control');
+    }
+  }
 
+  /**
+   * Dynamic alter method for "links__dropbutton".
+   */
+  protected function alterLinksDropbutton() {
+    // Remove the 'dropbutton' suggestion.
+    array_shift($this->hookSuggestions);
+
+    $this->addSuggestion('bootstrap_dropdown');
+  }
+
+  /**
+   * Dynamic alter method for "user".
+   *
+   * @see https://www.drupal.org/node/2828634
+   * @see https://www.drupal.org/node/2808481
+   * @todo Remove/refactor once core issue is resolved.
+   */
+  protected function alterUser() {
+    $this->addSuggestionsForEntity('user');
+  }
+
+  /***************************************************************************
+   * Protected methods.
+   ***************************************************************************/
+
+  /**
+   * Add a suggestion to the list of suggestions.
+   *
+   * @param string $hook
+   *   The theme hook suggestion to add.
+   */
+  protected function addSuggestion($hook) {
+    $suggestions = $this->buildSuggestions($hook);
+    foreach ($suggestions as $suggestion) {
+      $this->suggestions[] = $suggestion;
+    }
   }
 
   /**
@@ -79,10 +166,6 @@ class ThemeSuggestions extends PluginBase implements AlterInterface {
    *
    * @see https://www.drupal.org/node/2808481
    *
-   * @param array $suggestions
-   *   The suggestions array.
-   * @param \Drupal\bootstrap\Utility\Variables $variables
-   *   The variables object.
    * @param string $entity_type
    *   Optional. A specific type of entity to look for.
    * @param string $prefix
@@ -91,14 +174,14 @@ class ThemeSuggestions extends PluginBase implements AlterInterface {
    *
    * @todo Remove/refactor once core issue is resolved.
    */
-  public function addEntitySuggestions(array &$suggestions, Variables $variables, $entity_type = 'entity', $prefix = '') {
+  protected function addSuggestionsForEntity($entity_type = 'entity', $prefix = '') {
     // Immediately return if there is no element.
-    if (!$variables->element) {
+    if (!$this->element) {
       return;
     }
 
     // Extract the entity.
-    if ($entity = $this->getEntity($variables, $entity_type)) {
+    if ($entity = $this->getEntityObject($entity_type)) {
       $entity_type_id = $entity->getEntityTypeId();
       // Only add the entity type identifier if there's a prefix.
       if (!empty($prefix)) {
@@ -107,7 +190,7 @@ class ThemeSuggestions extends PluginBase implements AlterInterface {
       }
 
       // View mode.
-      if ($view_mode = preg_replace('/[^A-Za-z0-9]+/', '_', $variables->element->getProperty('view_mode'))) {
+      if ($view_mode = preg_replace('/[^A-Za-z0-9]+/', '_', $this->element->getProperty('view_mode'))) {
         $suggestions[] = $prefix . $entity_type_id . '__' . $view_mode;
 
         // Bundle.
@@ -116,40 +199,164 @@ class ThemeSuggestions extends PluginBase implements AlterInterface {
           $suggestions[] = $prefix . $entity_type_id . '__' . $entity->bundle() . '__' . $view_mode;
         }
       }
-
-      // Ensure a unique array.
-      $suggestions = array_unique($suggestions);
     }
+  }
+
+  /**
+   * Builds a list of suggestions.
+   *
+   * @param string $hook
+   *   The theme hook suggestion to build.
+   *
+   * @return array
+   */
+  protected function buildSuggestions($hook) {
+    $suggestions = [];
+
+    $hook_suggestions = $this->hookSuggestions;
+
+    // Replace the first hook suggestion with $hook.
+    array_shift($hook_suggestions);
+    array_unshift($suggestions, $hook);
+
+    $suggestions = [];
+    while ($hook_suggestions) {
+      $suggestions[] = $hook . '__' . implode('__', $hook_suggestions);
+      array_pop($hook_suggestions);
+    }
+
+    // Append the base hook.
+    $suggestions[] = $hook;
+
+    // Return the suggestions, reversed.
+    return array_reverse($suggestions);
+  }
+
+  /**
+   * Retrieves the methods to invoke to process the theme hook suggestion.
+   *
+   * @return array
+   *   An indexed array of methods to be invoked.
+   */
+  protected function getAlterMethods() {
+    // Retrieve cached theme hook suggestion alter methods.
+    $cache = $this->theme->getCache('theme_hook_suggestions');
+    if ($cache->has($this->hook)) {
+      return $cache->get($this->hook);
+    }
+
+    // Uppercase each theme hook suggestion to be used in the method name.
+    $hook_suggestions = $this->hookSuggestions;
+    foreach ($hook_suggestions as $key => $suggestion) {
+      $hook_suggestions[$key] = Unicode::ucfirst($suggestion);
+    }
+
+    $methods = [];
+    while ($hook_suggestions) {
+      $method = 'alter' . implode('', $hook_suggestions);
+      if (method_exists($this, $method)) {
+        $methods[] = $method;
+      }
+      array_pop($hook_suggestions);
+    }
+
+    // Reverse the methods.
+    $methods = array_reverse($methods);
+
+    // Cache the methods.
+    $cache->set($this->hook, $methods);
+
+    return $methods;
   }
 
   /**
    * Extracts the entity from the element(s) passed in the Variables object.
    *
-   * @param \Drupal\bootstrap\Utility\Variables $variables
-   *   The Variables object.
    * @param string $entity_type
    *   Optional. The entity type to attempt to retrieve.
    *
    * @return \Drupal\Core\Entity\EntityInterface|null
    *   The extracted entity, NULL if entity could not be found.
    */
-  public function getEntity(Variables $variables, $entity_type = 'entity') {
+  protected function getEntityObject($entity_type = 'entity') {
     // Immediately return if there is no element.
-    if (!$variables->element) {
+    if (!$this->element) {
       return NULL;
     }
 
     // Attempt to retrieve the provided element type.
-    $entity = $variables->element->getProperty($entity_type);
+    $entity = $this->element->getProperty($entity_type);
 
     // If the provided entity type doesn't exist, check to see if a generic
     // "entity" property was used instead.
     if ($entity_type !== 'entity' && (!$entity || !($entity instanceof EntityInterface))) {
-      $entity = $variables->element->getProperty('entity');
+      $entity = $this->element->getProperty('entity');
     }
 
     // Only return the entity if it's the proper object.
     return $entity instanceof EntityInterface ? $entity : NULL;
+  }
+
+  /**
+   * Processes the necessary theme hook suggestions.
+   */
+  protected function processSuggestions() {
+    // Add special hook suggestions for Bootstrap panels.
+    if ((in_array($this->originalHook, $this->bootstrapPanelTypes)) && $this->element && $this->element->getProperty('bootstrap_panel', TRUE)) {
+      $this->addSuggestion('bootstrap_panel');
+    }
+
+    // Retrieve any dynamic alter methods.
+    $methods = $this->getAlterMethods();
+    foreach ($methods as $method) {
+      $this->$method();
+    }
+  }
+
+  /***************************************************************************
+   * Deprecated methods (DO NOT USE).
+   ***************************************************************************/
+
+  /**
+   * Adds "bundle" and "view mode" suggestions for an entity.
+   *
+   * @param array $suggestions
+   *   The suggestions array, this is ignored.
+   * @param \Drupal\bootstrap\Utility\Variables $variables
+   *   The variables object, this is ignored.
+   * @param string $entity_type
+   *   Optional. A specific type of entity to look for.
+   * @param string $prefix
+   *   Optional. A prefix (like "entity") to use. It will automatically be
+   *   appended with the "__" separator.
+   *
+   * @deprecated Since 8.x-3.2. Will be removed in a future release.
+   *
+   * @see \Drupal\bootstrap\Plugin\Alter\ThemeSuggestions::addSuggestionsForEntity
+   */
+  public function addEntitySuggestions(array &$suggestions, Variables $variables, $entity_type = 'entity', $prefix = '') {
+    Bootstrap::deprecated();
+    $this->addSuggestionsForEntity($entity_type, $prefix);
+  }
+
+  /**
+   * Extracts the entity from the element(s) passed in the Variables object.
+   *
+   * @param \Drupal\bootstrap\Utility\Variables $variables
+   *   The Variables object, this is ignored.
+   * @param string $entity_type
+   *   Optional. The entity type to attempt to retrieve.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   *   The extracted entity, NULL if entity could not be found.
+   *
+   * @deprecated Since 8.x-3.2. Will be removed in a future release.
+   *
+   * @see \Drupal\bootstrap\Plugin\Alter\ThemeSuggestions::getEntityObject
+   */
+  public function getEntity(Variables $variables, $entity_type = 'entity') {
+    Bootstrap::deprecated();
+    return $this->getEntityObject($entity_type);
   }
 
 }
--- src/Utility/Storage.php
+++ src/Utility/Storage.php
@@ -192,6 +192,12 @@ class Storage extends MemoryStorage implements \Iterator {
 
   /**
    * Saves the data back to the database, if necessary, on shutdown.
+   *
+   * This method is automatically invoked during PHP shutdown.
+   *
+   * @internal
+   *
+   * @see \Drupal\bootstrap\Utility\Storage::__construct
    */
   public function save() {
     if ($this->changed) {
