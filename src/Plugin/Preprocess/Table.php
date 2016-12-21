d4342663425e59a0adde3e992d5facaeebe3971a
Mark Carver <mark.carver@me.com>
2016-12-21 00:28:10 -0600

    Issue #2838220 by markcarver: Passed table contexts are ignored

--- src/Plugin/Preprocess/Table.php
+++ src/Plugin/Preprocess/Table.php
@@ -22,18 +22,22 @@ class Table extends PreprocessBase {
    * {@inheritdoc}
    */
   public function preprocessVariables(Variables $variables) {
-    $responsive = (string) $this->theme->getSetting('table_responsive');
-    switch ($responsive) {
-      case '-1':
-        $variables['responsive'] = !\Drupal::service('router.admin_context')->isAdminRoute();
-        break;
-      case '0':
-        $variables['responsive'] = FALSE;
-        break;
-      case '1':
-        $variables['responsive'] = TRUE;
-        break;
-    }
+    // Bordered.
+    $variables['bordered'] = !!$variables->getContext('bordered', $this->theme->getSetting('table_bordered'));
+
+    // Condensed.
+    $variables['condensed'] = !!$variables->getContext('condensed', $this->theme->getSetting('table_condensed'));
+
+    // Hover.
+    $variables['hover'] = !!$variables->getContext('hover', $this->theme->getSetting('table_hover'));
+
+    // Striped.
+    $variables['striped'] = empty($variables['no_striping']) && $variables->getContext('striped', $this->theme->getSetting('table_striped'));
+    unset($variables['no_striping']);
+
+    // Responsive.
+    $responsive = $variables->getContext('responsive', $this->theme->getSetting('table_responsive'));
+    $variables['responsive'] = $responsive == -1 ? !\Drupal::service('router.admin_context')->isAdminRoute() : !!(int) $responsive;
   }
 
 }
