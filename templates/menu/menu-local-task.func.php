e7b82383ec73e2aac836d35083574428fffe8d63
Mark Carver <mark.carver@me.com>
2017-04-04 15:42:17 -0500

    Invoke theme_menu_local_task() from bootstrap_menu_local_task()
    Replace with core's theme_menu_local_task() implementation.
    
    Issue #2866798

--- templates/menu/menu-local-task.func.php
+++ templates/menu/menu-local-task.func.php
@@ -22,26 +22,5 @@
  * @ingroup theme_functions
  */
 function bootstrap_menu_local_task($variables) {
-  $link = $variables['element']['#link'];
-
-  $options = isset($link['localized_options']) ? $link['localized_options'] : array();
-
-  // Filter the title if the "html" is not set, otherwise l() will automatically
-  // sanitize using check_plain(), so no need to call that here.
-  $title = empty($options['html']) ? filter_xss_admin($link['title']) : $link['title'];
-
-  $href = $link['href'];
-  $attributes = array();
-
-  // Add text to indicate active tab for non-visual users.
-  if (!empty($variables['element']['#active'])) {
-    $options['html'] = TRUE;
-    $attributes['class'][] = 'active';
-    $title = t('!local-task-title!active', array(
-      '!local-task-title' => $title,
-      '!active' => '<span class="element-invisible">' . t('(active tab)') . '</span>',
-    ));
-  }
-
-  return '<li' . drupal_attributes($attributes) . '>' . l($title, $href, $options) . "</li>\n";
+  return theme_menu_local_task($variables);
 }
