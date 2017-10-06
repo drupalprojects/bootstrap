964ab6a3545d4dcae40f6c44d8ca8cd3bf61b577
Mark Carver <mark.carver@me.com>
2017-10-06 04:06:34 -0500

    Issue #2686247 by alexdmccabe, markcarver: Ajax not working in dropbuttons (operation links)

--- src/Plugin/Preprocess/BootstrapDropdown.php
+++ src/Plugin/Preprocess/BootstrapDropdown.php
@@ -70,8 +70,8 @@ class BootstrapDropdown extends PreprocessBase implements PreprocessInterface {
             '#type' => 'link',
             '#title' => $element['title'],
             '#url' => $element['url'],
-            '#ajax' => $element['ajax'] ?? [],
-            '#attributes' => $element['attributes'] ?? [],
+            '#ajax' => isset($element['ajax']) ? $element['ajax'] ? [],
+            '#attributes' => isset($element['attributes']) ? $element['attributes'] ? [],
           ];
         }
       }
